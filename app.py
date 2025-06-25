65b0077aebbc58d3e0a4c4d2800da779d48c6199
seicher1 <silkalnsa@gmail.com>
2025-06-25 12:50:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -79,3 +79,8 @@ def index():
         return send_file(output_path, as_attachment=True)
 
     return render_template('index.html')
+    
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
